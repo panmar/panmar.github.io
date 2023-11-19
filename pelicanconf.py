0b1b8cce3fdde42950769c6cab628171a89a401a
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2023-11-19 21:44:20 +0100

    Update pelican config for statics to include folder

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,7 +36,7 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-STATIC_PATHS = ['static']
+STATIC_PATHS = ['static', 'images']
 
 THEME = 'theme'
 OUTPUT_PATH = 'docs/'
